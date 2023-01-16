633d2bf5e5ff7927ca5e299829cf16b1fd0d9cc8
Elvis-Almeida-Mendes-Junior <103849339+Elvis-Almeida-Mendes-Junior@users.noreply.github.com>
2023-01-15 23:25:30 -0300

    add comentarios

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 //toda vez que apertar um botão o valor do mesmo seja inpreço na div de resposta;
 function insert(num){
-    var res = document.getElementById('resultado');
+    var res = document.getElementById('resultado');//busca a div e adiciona o valor de cada button
     res.innerHTML +=num
 }
 function clean(){
-    document.getElementById('resultado').innerHTML = ''
+    document.getElementById('resultado').innerHTML = ''//aciona a função C da calculadora
 }
 function back(){
     var resultado = document.getElementById('resultado').innerHTML
-    document.getElementById('resultado').innerHTML = resultado.substring(0,resultado.length -1);
+    document.getElementById('resultado').innerHTML = resultado.substring(0,resultado.length -1);//permite apagar apenas o numero desejado
 }
 
 function calcular(){
     var resultado = document.getElementById('resultado').innerHTML;
     if(resultado){
-        document.getElementById('resultado').innerHTML = eval(resultado)
+        document.getElementById('resultado').innerHTML = eval(resultado)//calcula o valor indicado pela div
     }
 }
